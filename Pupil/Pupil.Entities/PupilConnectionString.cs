2f0f64d5114008c4f62c2f87893fd60e901628a9
Erik <Erik@EEFMAIN>
2018-01-14 18:32:31 -0500

    add entity framework

---
+++ Pupil/Pupil.Entities/PupilConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pupil.Entities
+{
+    public partial class PupilEntities : DbContext
+    {
+        public PupilEntities(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
---
+++ Pupil/Pupil.Prototype/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Pupil.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pupil.Prototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            using (var db = new PupilEntities(Properties.Settings.Default.DesktopPupilConnection))
+            {
+                foreach(Course course in db.Courses)
+                {
+                    db.Courses.Remove(course);
+                    db.SaveChanges();
+                }
+
+                Course course1 = new Course{ CourseNumber = "CS-200", Title = "Introduction to Java", Capacity = 35, DateCreated = DateTime.Now, DateModified = DateTime.Now, IsActive = true };
+
+                db.Courses.Add(course1);
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
